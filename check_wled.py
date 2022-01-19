8cddeb1f087857ad4dd6aafb1deb8276733a2409
mdiazgoncalves <mcdg@outlook.pt>
2022-01-19 10:24:13 +0000

    clean code

--- check_wled.py
+++ check_wled.py
@@ -7,7 +7,7 @@ import argparse
 import requests
 
 # Return codes expected by Nagios
-codes = ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN']
+CODES = ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN']
 
 # Create the parser
 my_parser = argparse.ArgumentParser(description='Check WLED node availability and status')
@@ -18,21 +18,27 @@ my_parser.add_argument('hostname', metavar='<hostname>', type=str, help='The hos
 # Execute the parse_args() method
 args = my_parser.parse_args()
 
+
 # Check logic starts here
+def main():
 
-try:
-    response = requests.get('http://' + args.hostname + '/json')
-    response.raise_for_status()
-except requests.exceptions.RequestException  as e:
-    STATUS = 2
-    MESSAGE =str(e)
-else:
-    data = response.json()
-    STATUS = 0
-    MESSAGE = f"name:{data['info']['name']} on:{data['state']['on']} mac_address:{':'.join(data['info']['mac'][i:i+2] for i in range(0,12,2))} version:{data['info']['ver']}"
+    try:
+        response = requests.get('http://' + args.hostname + '/json')
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        STATUS = 2
+        MESSAGE = str(e)
+    else:
+        data = response.json()
+        STATUS = 0
+        MESSAGE = f"name:{data['info']['name']} on:{data['state']['on']} mac_address:{':'.join(data['info']['mac'][i:i+2] for i in range(0,12,2))} version:{data['info']['ver']}"
 
-# Print the message for nagios
-print(f"{codes[STATUS]} - {MESSAGE}")
+    # Print the message for nagios
+    print(f"{CODES[STATUS]} - {MESSAGE}")
 
-# Exit with status code
-raise SystemExit(STATUS)
+    # Exit with status code
+    raise SystemExit(STATUS)
+
+
+if __name__ == "__main__":
+    main()
