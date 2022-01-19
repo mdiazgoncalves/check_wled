199be53aa9488c111c8571aa380f6a4bb3f91829
mdiazgoncalves <mcdg@outlook.pt>
2022-01-19 10:33:22 +0000

    clean code

--- check_wled.py
+++ check_wled.py
@@ -25,19 +25,19 @@ def main():
     try:
         response = requests.get('http://' + args.hostname + '/json')
         response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        STATUS = 2
-        MESSAGE = str(e)
+    except requests.exceptions.RequestException as exception:
+        status = 2
+        message = str(exception)
     else:
         data = response.json()
-        STATUS = 0
-        MESSAGE = f"name:{data['info']['name']} on:{data['state']['on']} mac_address:{':'.join(data['info']['mac'][i:i+2] for i in range(0,12,2))} version:{data['info']['ver']}"
+        status = 0
+        message = f"name:{data['info']['name']} on:{data['state']['on']} mac_address:{':'.join(data['info']['mac'][i:i+2] for i in range(0,12,2))} version:{data['info']['ver']}"
 
     # Print the message for nagios
-    print(f"{CODES[STATUS]} - {MESSAGE}")
+    print(f"{CODES[status]} - {message}")
 
     # Exit with status code
-    raise SystemExit(STATUS)
+    raise SystemExit(status)
 
 
 if __name__ == "__main__":
